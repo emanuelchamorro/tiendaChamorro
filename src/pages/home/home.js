392c5df976bd12205fb8f56e2bc29f5b563f876a
emanuelchamorro <emanuel.chamorro@gmail.com>
2021-06-15 11:30:42 -0300

    clase 3 - Agregar navbar al proyecto

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,11 +3,11 @@ import {NavBar} from '../../components/navBar/navBar'
 export const Home = () => {
  
     let name = 'Tienda Domo';
-    
+
     return (
       <>
          <NavBar brand={name} />
-         <h2>Las ofertas de la semana</h2>
+        
       </>
     );
   }
