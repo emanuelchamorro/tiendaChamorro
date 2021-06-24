fc83d42f28f76bcd755d28b0b07293b77ca1ed50
emanuelchamorro <emanuel.chamorro@gmail.com>
2021-06-24 18:00:54 -0300

    Reentrega Desafio Clase 5

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -2,13 +2,14 @@ import './itemCount.css'
 import { useState} from "react";
 
 
-export function ItemCount({ stock, initial }) {
+export function ItemCount({ stock, initial, onAdd}) {
     
-    const [value, setValue] = useState(initial)
+    const [value, setValue] = useState(stock > 0 ? initial : 0)
 
-    const onAdd = (operator) => {
+    const valueIncrement = (operator) => {
     
     let newValue 
+
      switch(operator) {
          case '+':    
           newValue = Number(value) + 1
@@ -21,22 +22,21 @@ export function ItemCount({ stock, initial }) {
         default:
             break
      }
-  
+
      if(newValue <= stock && newValue > 0){
      setValue(newValue)
     } else {
      return ''
     }
-
 }
    return (
      <>
      <div className="btn-container-increment">
-      <button className ="btn-increment" onClick={()=> stock > 0 ? onAdd('-') : ''}>-</button>
+      <button className ="btn-increment" onClick={()=> stock > 0 ? valueIncrement('-') : ''}>-</button>
       <input className="value-increment" type="text" onChange={(e)=>setValue(e.target.value)} value={value}></input>
-      <button className ="btn-increment" onClick={()=> stock > 0 ? onAdd('+') : ''}>+</button> 
+      <button className ="btn-increment" onClick={()=> stock > 0 ? valueIncrement('+') : ''}>+</button> 
      </div>
-     <button className="btn-add-to-cart">Agregar al carrito</button>
+     <button className="btn-add-to-cart" disabled={stock === '0' ? true : false} onClick={onAdd}>Agregar al carrito</button>
      </>     
 
     )
